654086a140c93d58659dc3fffc709704ed2b7b1a
Eric Liu <ericl@amayasoftware.com>
2020-01-20 10:09:43 -0500

    adding c7

--- web/javascript/chapter7 jquery/jquery.js
+++ web/javascript/chapter7 jquery/jquery.js
@@ -36,5 +36,68 @@ $(function(){
     // find li element that contains pine
     $('li:contains("pine")').text('almonds');
 
-    $('li#one').remove();
+    //$('li#one').remove();
+
+    // update attributes
+    $('li#one').removeClass();
+    $('li#one').addClass('hot');
+
+    $('li#one').attr('id', 'five');
+    $('li#five').attr('id', 'one');
+    //$('li#five').removeAttr('id');
+
+    // get/set css rules
+    var coler = $('li#one').css('color');
+    console.log('id one color=' + coler);
+
+    $('li').css('padding-left', '+=20');
+    $('li').css({
+        'background-color': '#d3d3d3',
+        'font-family' : 'Courier'
+    });
+
+    $listHTML = $('ul').html();
+    console.log('ul html=' + $listHTML);
+
+    // .each() loop thru all elements
+    $('li').each(function(){
+        var ids = this.id;
+        $(this).append('<em class="complete">' + ' ' + ids + '</em>');
+    });
+
+    // events
+    var $listItems = $('li');
+    $listItems.on('mouseover', function(e){
+        var ids = this.id;
+        $listItems.children('span').remove();
+        $(this).append(' <span class="mouseon">' + ids + ' ' + e.type + '</span>');
+        //console.log("current li=" + $(this).html());
+    });
+
+    $listItems.on('mouseout', function(){
+        $listItems.children('span').remove();
+    });
+
+   /* $('ul').on('click', ':not(#four)', {status: 'important'}, function(e){
+        var listItem = 'item: ' + e.target.textContent + '<br />';
+        var itemStatus = 'Status: ' + e.data.status + '<br />';
+        var eventType = 'Event: ' + e.type;
+        $('#notes').html(listItem + itemStatus + eventType);
+    });
+    */
+
+    $('li').hide().each(function(index){
+        $(this).delay(700 * index).fadeIn(700);
+    });
+
+    // click to remove with animation
+    $('li').on('click', function(){
+        $(this).animate(
+            {   opacity: 0.0,
+                paddingLeft: '+=80'
+            },
+            500, function(){
+                $(this).remove();
+        });
+    });
 });
